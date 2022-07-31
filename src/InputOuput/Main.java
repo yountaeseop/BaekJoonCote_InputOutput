e5c6c61d97d8041a69efd5f7d2c425830b448772
yountaeseop <jangmj6931@gmail.com>
2022-07-31 20:32:13 +0900

    특수문자로 고양이 출력하기(역슬래시)

---
+++ src/InputOuput/Main.java
@@ -0,0 +1,17 @@
+package InputOuput;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+				
+	}
+	//자바 특수문자 입출력표 보기
+}
+
+// 제출할때, 패키지는 빼고 제출해야한다.
+// 클래스이름을 반드시 Main, main으로 해야한다.
