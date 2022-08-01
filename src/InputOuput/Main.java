37ffc98a37116a040ef8722501f5d11051edecd7
yountaeseop <jangmj6931@gmail.com>
2022-08-01 20:57:41 +0900

    특수문자로 강아지 출력하기

--- src/InputOuput/Main.java
+++ src/InputOuput/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
 				
 	}
 	//자바 특수문자 입출력표 보기
